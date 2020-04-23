20d27066cfc965224ed46af0a7351239b768c46e
Awilliams4321 <awilliams1234@live.com>
2020-04-23 08:10:27 -0600

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,2 +1,30 @@
 class Show < ActiveRecord::Base
+    def Show::highest_rating
+        Show.maximum("rating")
+    end
+
+    def Show::most_popular_show
+        Show.order(rating: :desc).first
+    end
+
+    def Show::lowest_rating
+
+    end 
+
+    def Show::least_popular_show
+
+    end 
+
+    def Show::ratings_sum
+
+    end 
+
+    def Show::popular_shows
+
+    end 
+
+    def Show::shows_by_alphabetical_order
+
+    end
+
 end
